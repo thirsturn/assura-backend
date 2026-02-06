e1fbf93f99163361e190286540b1723f3da1c5ce
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-02-06 17:39:25 +0530

    ..

--- backend.Core/obj/Debug/net8.0/backend.Core.AssemblyInfo.cs
+++ backend.Core/obj/Debug/net8.0/backend.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16073d2747c986a1f6c88121e7863c4a9cbf166b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b4bd377173219079e961fda372537a39058e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
